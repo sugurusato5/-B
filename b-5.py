6f70ffa6520059704ab95f6a732d2f7aa906de8d
sugurusato5 <jt.ag.dm.2782@gmail.com>
2025-09-25 16:04:26 +0900

    課題B修正

--- b-5.py
+++ b-5.py
@@ -1,21 +1,39 @@
-num_list = input("データを入力してください(スペース区切り) >").split(" ")
-total = 0
-for num in num_list:
-    total += int(num)
+num_list = [int(number) for number in input("データを入力してください(スペース区切り) >").split()]
 
-print(f"合計値: {total}")
 
-max = num_list[0]
-for i in range(1, len(num_list)):
-    if num_list[i] > max:
-        max = num_list[i]
+# 合計
+def total(num_list):
+    total = 0
+    for num in num_list:
+        total += int(num)
 
-print(f"最大値: {max}")
+    return total
 
-min = num_list[0]
-for i in range(1, len(num_list)):
-    if num_list[i] < min:
-        min = num_list[i]
-print(f"最小値: {min}")
 
-print(f"平均値: {total // len(num_list)}")
+def max(num_list):
+    max = num_list[0]
+    for i in num_list:
+        if i > max:
+            max = i
+    return max
+
+
+def min(num_list):
+    min = num_list[0]
+    for i in num_list:
+        if i < min:
+            min = i
+    return min
+
+
+def ave(num_list):
+    total = 0
+    for num in num_list:
+        total += num
+    return total // len(num_list)
+
+
+print(f"合計値: {total(num_list)}")
+print(f"合計値: {max(num_list)}")
+print(f"合計値: {min(num_list)}")
+print(f"合計値: {ave(num_list)}")
